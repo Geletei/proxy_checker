463201b06ab76bb97d6db25c3433cbe5699162c9
vmytsai <vlad.mytsai@gmail.com>
2025-02-03 20:58:51 +0200

    Add files

---
+++ main.py
@@ -0,0 +1,121 @@
+import os
+import asyncio
+import aiohttp
+import platform
+from datetime import datetime
+from tabulate import tabulate
+from settings import proxy_type, do_report, country, multi
+
+clear_text = 'cls' if platform.system().lower() == 'windows' else 'clear'
+
+info = {}
+good_proxies = []
+bad_proxies = []
+country_report = []
+
+
+async def print_data():
+    os.system(clear_text)
+    headers = ['#', f'Work', 'IP', 'Timeout', 'Country', 'City']
+    table = [[k, i['work'], i['ip'], i['timeout'], i['country'], i['city']] for k, i in info.items()]
+
+    final_table = tabulate(table, headers, tablefmt='mixed_outline', stralign='center', numalign='center')
+    print(final_table)
+
+
+async def set_proxy(proxy_str):
+    if '@' in proxy_str:
+        if '//' in proxy_str:
+            proxy = proxy_str.split('//')[1]
+        else:
+            proxy = proxy_str
+        proxy_address = str(proxy.split('@')[1]).split(':')[0]
+    else:
+        proxy_things = proxy_str.split(':')
+        if len(proxy_things) == 2:
+            proxy_address = proxy_things[0]
+            proxy_port = proxy_things[1]
+            proxy = f'{proxy_address}:{proxy_port}'
+        else:
+            proxy_login = proxy_things[2]
+            proxy_password = proxy_things[3]
+            proxy_address = proxy_things[0]
+            proxy_port = proxy_things[1]
+            proxy = f'{proxy_login}:{proxy_password}@{proxy_address}:{proxy_port}'
+
+    proxy_final = f'{proxy_type}://{proxy}'
+    return proxy_address, proxy_final
+
+
+async def read_proxies():
+    with open("proxies.txt", "r") as file:
+        n = 0
+        for row in file.readlines():
+            n += 1
+            proxy_base = row.strip()
+            if proxy_base:
+                ip, proxy = await set_proxy(proxy_base)
+                info[str(n)] = {
+                    'ip': ip,
+                    'base_format': proxy_base,
+                    'proxy': proxy,
+                    'work': '🍋\u200a',
+                    'timeout': '░░░░░',
+                    'country': '░░',
+                    'city': '░░░░░░░░░░'
+                }
+
+
+async def checker(n, data):
+    proxies = data['proxy']
+    time_start = datetime.now()
+    try:
+        url = 'http://ip-api.com/json'
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, proxy=proxies) as response:
+                result = await response.json()
+                if result['status'] == 'success':
+                    info[n]['work'] = '🍏\u200a'
+                    good_proxies.append(data['base_format'])
+                    info[n]['ip'] = result['query']
+                    info[n]['country'] = result['countryCode']
+                    if country:
+                        if result['countryCode'] == country:
+                            country_report.append(data['base_format'])
+                    info[n]['city'] = result['city']
+                else:
+                    info[n]['work'] = '🍎\u200a'
+                    bad_proxies.append(data['base_format'])
+    except Exception:
+        info[n]['work'] = '🍎\u200a'
+        bad_proxies.append(data['base_format'])
+    time_end = datetime.now()
+    time_res = time_end - time_start
+    print()
+    info[n]['timeout'] = str(round(time_res.total_seconds(), 3))
+    await print_data()
+
+
+async def main():
+    await read_proxies()
+    await print_data()
+    if multi:
+        await asyncio.gather(*[checker(n, data) for n, data in info.items()])
+    else:
+        for n, data in info.items():
+            await checker(n, data)
+
+    if do_report:
+        with open('good_proxies.txt', 'w') as file:
+            file.write('\n'.join(good_proxies))
+
+        with open('bad_proxies.txt', 'w') as file:
+            file.write('\n'.join(bad_proxies))
+
+    if country:
+        with open('country_report.txt', 'w') as file:
+            file.write('\n'.join(country_report))
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ settings.py
@@ -0,0 +1,6 @@
+proxy_type = 'http'  # 'http' | 'socks5'
+multi = True  # True | False
+
+do_report = False  # True | False
+
+country = ''  # 'UA' | 'GB' | ..
